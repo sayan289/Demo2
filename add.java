a5cbb2fc48e0f8ed4913226756ca354c49c34f0e
Subhajitgarai <subhajitgarai12345@gmail.com>
2024-01-09 09:44:34 +0530

    Added two number addition by userinput

---
+++ add.java
@@ -0,0 +1,15 @@
+package Sayan_Github.Demo2;
+
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first Number:");
+        int a=sc.nextInt();
+        System.out.println("Enter second number:");
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("Addition Result = "+c);
+    }
+}
