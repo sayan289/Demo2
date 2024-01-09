c070a36c52c2ea9375d741a806f8a303fdebee31
sayan289 <sayanadhikary289@gmail.com>
2024-01-09 10:01:56 +0530

    Changed perform

--- addtwonumber.java
+++ addtwonumber.java
@@ -4,6 +4,6 @@ public class addtwonumber {
     public static void main(String ars[])
     {
         int a=5,b=5;
-        System.out.println(a+b);
+        System.out.println("The output is :"+(a+b));
     }
 }
